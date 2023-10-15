a944a014e7304e361d13912f85e1d9bf0779c722
gaurav20200 <gauravbadhani86@gmail.com>
2023-10-16 01:31:55 +0530

    app ignited

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"This is the heading1")));
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"This is the heading1"),React.createElement("h2",{},"This is the heading2")]));
 const heading = React.createElement("h1",{},"Hello World from React!");
     const root = ReactDOM.createRoot(document.getElementById("root"));
     root.render(parent);
