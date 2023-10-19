e01ad6379b0877fde29b321b9d14850b941d3498
gaurav20200 <gauravbadhani86@gmail.com>
2023-10-20 01:34:19 +0530

    Component Practice

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
-import React from "react";
-import  ReactDOM  from "react-dom/client";
-const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"This is the heading1"),React.createElement("h2",{},"This is the heading2")]));
-const heading = React.createElement("h1",{},"Hello World from React!");
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+import React, { createElement } from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement("h1",{id:"heading"},"Namaste  React");
+//babel package transpiles
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const Hola =() =>( (<h1>THis is best</h1>));
+const HeadingComponent =() =>(<div id="container">
+    <Hola/>   //one component insider another component is invoked this way
+    {}     //curly braces can be used to run any javascript code 
+<h1 className="head">welcome to mysdfdf f ssdfsfddfdsdfsfsffsdfds hsdfdsfouse</h1>
+</div>);
+const AnotherComponent = () =>(<div id="container">
+<Hola/>   
+<h1 className="tail">AnotherComponent is here</h1>
+</div>);
+
+
+root.render(<HeadingComponent/>); 
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # React
+
+
+#parcel 
+-Dev Build
+-Local Server
+-MMR = Hot Module Replacement
+-File Watching Algorithm
+-Caching -Faster Builds
+-Image Optimization
+-Minification
+-Bundling
+-Compressing
+-consistent Hashing
+-code splitting
+-tree shaking
